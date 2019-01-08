1820d7a85f6b49d69cae0959b3c9b607c0e5ba98
Tushar Goyal <tushar.chapter247@gmail.com>
2019-01-08 16:19:38 +0530

    Add bootstrap to project. Remove unrequired css

--- src/containers/root/app.jsx
+++ src/containers/root/app.jsx
@@ -4,20 +4,8 @@ import './app.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="app-container">
+        
       </div>
     );
   }
