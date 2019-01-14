44f9fe49af1423038eaa49213e12d28def2311ee
Tushar Goyal <tushar.chapter247@gmail.com>
2019-01-14 14:20:55 +0530

    install package prop-types to check prop value types

--- src/components/burger/burger-ingredients/burger-ingredients.component.jsx
+++ src/components/burger/burger-ingredients/burger-ingredients.component.jsx
@@ -1,5 +1,9 @@
 import React from 'react'
 
-const BurgerIngredients = () => {};
+const BurgerIngredients = (props) => {
+  let ingredients = null;
+
+  console.log('ingredients type: ', props.type);
+};
 
 export default BurgerIngredients;
