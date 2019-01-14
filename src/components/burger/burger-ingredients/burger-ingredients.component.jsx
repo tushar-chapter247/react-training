730af279bb10b2324219f566526ce85ec5152a65
Tushar Goyal <tushar.chapter247@gmail.com>
2019-01-14 14:29:35 +0530

    Implement conditions and css to burger ingredients component

--- src/components/burger/burger-ingredients/burger-ingredients.component.jsx
+++ src/components/burger/burger-ingredients/burger-ingredients.component.jsx
@@ -1,10 +1,28 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'; // prop types can only work in class component
+import './burger-ingredients.css';
 
-class BurgerIngredients extends Compnent {
+class BurgerIngredients extends Component {
   render() {
 
     let ingredients = null;
+
+    switch (this.props.type) {
+      case 'meat':
+        ingredient = (<div className='meat' />);
+        break;
+      case 'cheese':
+        ingredient = (<div className='cheese' />);
+        break;
+      case 'salad':
+        ingredient = (<div className='salad' />);
+        break;
+      case 'bacon':
+        ingredient = (<div className='beacon' />);
+        break;
+      default:
+        ingredient = null;
+    }
 
     return ingredients;
 
