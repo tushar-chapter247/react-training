b87167dd20ccfd067522d6c5dd75b72189839fa0
Tushar Goyal <tushar.chapter247@gmail.com>
2019-01-17 14:51:06 +0530

    create two components calendar and range-calendar

---
+++ src/containers/calendar/index.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class Calendar extends Component {
+  constructor() {
+    super();
+  }
+
+
+  render() {
+    return (
+      <>
+        <h2>this is calendar page</h2>
+      </>
+    )
+  }
+};
+
+export default Calendar;
---
+++ src/containers/range-calendar/index.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class RangeCalendar extends Component {
+  constructor() {
+    super()
+  }
+
+
+
+  render() {
+    return (
+      <>
+        <h2>this is range calendar page</h2>
+      </>
+    )
+  }
+
+};
+
+export default RangeCalendar;
