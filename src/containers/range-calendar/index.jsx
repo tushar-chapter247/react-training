83889af2d9bd2951bfe10343c1e19ce17ca0c51f
Tushar Goyal <tushar.chapter247@gmail.com>
2019-01-19 15:04:08 +0530

    implement range calendar

--- src/containers/range-calendar/index.jsx
+++ src/containers/range-calendar/index.jsx
@@ -1,11 +1,61 @@
 import React, { Component } from 'react'
+import './range-calendar.css';
+import $ from 'jquery';
+import moment from "moment";
 
 class RangeCalendar extends Component {
+  constructor() {
+    super();
+    this.state = {
+      startDate: moment().format("DD-MM-YYYY"),
+      endDate: moment().format("DD-MM-YYYY")
+    };
+    this.rangeCal = React.createRef();
+  }
+
+
+  componentDidMount = () => {
+    this.$rangeCal = $(this.rangeCal);
+    this.$rangeCal.fullCalendar({
+      defaultView: 'month',
+      header: {
+        left: "prev,next today myCustomButton",
+        center: "title",
+        right: "month"
+      },
+      defaultDate: moment(),
+      height: 500,
+      showNonCurrentDates: true,
+      selectable: true,
+      unselectAuto: false,
+      select: this.handleSelect,
+    });
+  };
+
+
+  componentWillUnmount() {
+    this.$rangeCal.fullCalendar('destroy');
+  }
+
+
+  handleSelect = (start, end, event, view) => {
+    this.setState({
+      startDate: start.format("DD-MM-YYYY"),
+      endDate: end.subtract(1, 'days').format("DD-MM-YYYY")
+    })
+  }
+
+
   render() {
     return (
-      <>
-        <h2>this is range calendar page</h2>
-      </>
+      <div className="range-calendar-container">
+        <h2>Range Calendar</h2>
+        <br/>
+        <h3>Selected range: {this.state.startDate} - {this.state.endDate}</h3>
+        <br/>
+        {/* calendar empty div */}
+        <div className="range-calendar-section" ref={el => this.rangeCal = el} />
+      </div>
     )
   }
 
