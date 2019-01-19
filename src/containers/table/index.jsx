f06d5ca055db39b4a218e4b9aa4210e3ed8231f4
Tushar Goyal <tushar.chapter247@gmail.com>
2019-01-19 15:41:37 +0530

    Implement dummy table

--- src/containers/table/index.jsx
+++ src/containers/table/index.jsx
@@ -1,12 +1,16 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import './table.css';
+import TableView from './component/table-component/table-component';
 
 class Table extends Component {
 
   render() {
     return (
-      <>
-        <h2>This is table page</h2>
-      </>
+      <div className="table-container">
+        <h2>Table</h2>
+        <br/>
+        <TableView />
+      </div>
     )
   }
 
