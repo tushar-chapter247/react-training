819f28f40b1e52ff4c040350637b787e1c215a6e
Tushar Goyal <tushar.chapter247@gmail.com>
2019-01-21 16:43:35 +0530

    Create basic structure with header component, going to install material

---
+++ src/App/components/header/Header.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Header extends Component {
+  render() {
+    return (
+      <>
+        <h2>This is Header Component</h2>
+      </>
+    );
+  }
+}
+
+
+export default Header;
